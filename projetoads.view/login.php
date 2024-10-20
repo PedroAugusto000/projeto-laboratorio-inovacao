99edf2b3b9c0ab8f8e00976b82888d1f30a9b7cd
Pedro Augusto Moura de Oliveira <pedroaugustomoura000@gmail.com>
2024-10-19 23:48:29 -0300

    Pequena adição de estilos no login.php (css básico)

--- projetoads.view/login.php
+++ projetoads.view/login.php
@@ -4,23 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="../css/stylesLogin.css">
 </head>
 <body>
-    <h2>Login</h2>
+
+ 
+    <h2 id="text-login">Login</h2>
+
+<div class="login">
     <form action="login.php" method="post">
+
         <div>
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
+            <label for="email">Usuário:</label>
+            <input type="usuario" id="usuario" name="usuario" required>
         </div> <br>
 
         <div>
             <label for="senha">Senha:</label>
             <input type="password" id="senha" name="senha" required>
-        </div>
+        </div> <br>
 
-        <div>
-            <button type="submit">Logar</button>
-        </div>
+
+        <button type="submit" id="button-login">Logar</button>
+
+</div>
 
     </form>
 </body>
